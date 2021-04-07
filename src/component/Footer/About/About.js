a1129ff355388ffd6709287cec4b6f412e24dfdc
tyonek <tyonek.thompson@gmail.com>
2021-04-07 19:53:34 -0400

    changed about us picture

--- src/component/Footer/About/About.js
+++ src/component/Footer/About/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Image , Col } from 'react-bootstrap';
-import about2 from '../../../assets/photos/about2.jpeg';
+import headshot from '../../../assets/photos/headshot.jpeg';
 import '../../../routes/LandingPage/LandingPage.css';
 
 
@@ -12,7 +12,7 @@ export default class About extends Component {
         <Container>
           <Row>
             <Col>
-              <Image className="aboutHeadshot img-fluid rounded" alt="Vanessa&Shannon" src={about2}  />
+              <Image className="aboutHeadshot img-fluid rounded" alt="Vanessa&Shannon" src={headshot} roundedCircle />
             </Col>
           </Row>
         </Container>
