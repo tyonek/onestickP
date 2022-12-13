40a97590a7fa8d4502ef9a1eb8453474f81cff4a
tyonek <tyonek.thompson@gmail.com>
2022-12-12 19:30:12 -0500

    fix

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -29,7 +29,7 @@ export default class LandingPage extends Component {
 				<h1 className="landingTitle">OneStick Phlebotomy Service</h1>
 				<h2 className="landingMotto">Simple as "One & Done"</h2>
 				<h2 className="landingMotto">To Apply Today!
-				<a href="tel:2672799461"><p>Call us at</p> (267)-279-9461</a> or <a href = "mailto: Contact@Onestickphlebotomy.com">Email</a>
+				<a className="courseDescription" href="tel:2672799461">Call</a> or <a href = "mailto: Contact@Onestickphlebotomy.com">Email</a>
 				</h2>
 				</div>
 
